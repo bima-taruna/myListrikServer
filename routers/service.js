5e1c24c2fe5f1d6f7437d9ff7063e8f11a202a33
bimataruna <bimataruna21@gmail.com>
2022-07-20 21:56:59 +0700

    mengubah path

--- routers/service.js
+++ routers/service.js
@@ -33,7 +33,7 @@ const storage = multer.diskStorage({
         if (isValid) {
             uploadError = null
         }
-      cb(uploadError, 'public/uploads')
+      cb(uploadError, __dirname)
     },
     filename: function (req, file, cb) {
       const fileName = file.originalname.split(' ').join('-');
