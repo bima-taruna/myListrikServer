990c5d1a6a2b234cf15610dfacc4b8caa7cdbeb3
bimataruna <bimataruna21@gmail.com>
2022-08-02 15:44:23 +0700

    edit put api

--- routers/service.js
+++ routers/service.js
@@ -48,20 +48,20 @@ router.put(`/:id`,authJwt,uploadOptions.single('icon'), async (req,res)=>{
             return res.status(401).json({message : 'anda tidak memiliki izin untuk mengakses laman ini',success:false});
         }
         let service = await Service.findById(req.params.id);
-        // let tes = () => {
-        //     if(req.file.path = undefined){
-        //        return service.icon
-        //     } else {
-        //        return basePath.secure_url
-        //     }
-        // }
+        let tes = () => {
+            if(!req.file.path){
+               return service.icon
+            } else {
+               return basePath.secure_url
+            }
+        }
         await cloudinary.uploader.destroy(service.cloudinary_id);
         const basePath = await cloudinary.uploader.upload(req.file.path);
         
         const serviceData =  {
             name : req.body.name || service.name,
             description : req.body.description || service.description,
-            icon : basePath.secure_url || service.icon,
+            icon : tes(),
             cloudinary_id : basePath.public_id || service.cloudinary_id
         };
         service = await Service.findByIdAndUpdate(req.params.id, serviceData, {new:true})
