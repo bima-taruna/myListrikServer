ae03b7da756effe4745b5c4de1f5d02ec8887b25
bimataruna <bimataruna21@gmail.com>
2022-09-06 16:00:56 +0700

    change order

--- routers/order.js
+++ routers/order.js
@@ -11,7 +11,7 @@ router.get(`/`,authJwt, async (req, res) => {
     const ordersList = await Order.find()
       .populate("user", "name")
       .populate("city", "name")
-      .populate("orderItems")
+      .populate("orderItems.service")
       .populate("teknisi", "name")
       .sort({ dateOrdered: -1 });
 
